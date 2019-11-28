751bd62241ad2564262487d4a16b95a2e26bcedf
Harris Butler <hbutler5@patriots.uttyler.edu>
2019-11-27 18:35:46 -0600

    Added login user login validation

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:qrscan/qrscan.dart' as scanner;
 import 'dart:typed_data';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 void main() => runApp(AttendanceTracker());
 
@@ -24,12 +31,35 @@ class ATLoginPage extends StatefulWidget {
 }
 
 class ATLoginPageState extends State<ATLoginPage> {
+  bool _isLoading = false;
   AnimationController _logoAnimationController;
   Animation<double> _logoAnimation;
+
+  TextEditingController emailController = new TextEditingController();
+  TextEditingController passwordController = new TextEditingController();
+
+  signIn(String email, password) async {
+    var jsonData = null;
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance(); 
+    var response = await http.post("https://attendance.page/api.php?type=0&student="+email+"&password="+password);
+    if(response.statusCode == 200)
+    {
+      jsonData = json.decode(response.body);
+      setState(() {
+        _isLoading = false; 
+        sharedPreferences.setString("token", jsonData['token']); 
+        Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => landingPage()), (Route<dynamic> route) => false);
+      }); 
+    }
+    else{
+      print(response.body); 
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false, 
       backgroundColor: Colors.black,
       body: Stack(
         fit: StackFit.expand,
@@ -58,24 +88,28 @@ class ATLoginPageState extends State<ATLoginPage> {
                     inputDecorationTheme: InputDecorationTheme(
                         labelStyle:
                             TextStyle(color: Colors.white60, fontSize: 20.0))),
-                child: Container(
+                child: _isLoading ? Center(child: CircularProgressIndicator()) : Container(
                   padding: const EdgeInsets.all(40.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
                       TextFormField(
+                        controller: emailController,
                         decoration: InputDecoration(
                           labelText: "Enter Email",
                         ),
                         keyboardType: TextInputType.emailAddress,
                       ),
+
                       TextFormField(
+                        controller: passwordController,
                         decoration: InputDecoration(
                           labelText: "Password",
                         ),
                         keyboardType: TextInputType.text,
                         obscureText: true,
                       ),
+
                       Padding(padding: const EdgeInsets.only(top: 20.0)),
                       MaterialButton(
                         height: 40.0,
@@ -84,7 +118,10 @@ class ATLoginPageState extends State<ATLoginPage> {
                         textColor: Colors.white,
                         child: Text("Login"),
                         onPressed: () {
-                          Navigator.pushNamed(context, '/landingPage');
+                          setState(() {
+                            _isLoading = true;
+                          }); 
+                          signIn(emailController.text, passwordController.text);
                         },
                         splashColor: Colors.white60,
                       ),
@@ -109,6 +146,7 @@ class landingPage extends StatefulWidget {
 class _landingPageState extends State<landingPage> {
   String barcode = '';
   Uint8List bytes = Uint8List(200);
+  SharedPreferences sharedPreferences; 
 
   Future _scan() async {
     String barcode = await scanner.scan();
@@ -118,12 +156,31 @@ class _landingPageState extends State<landingPage> {
   @override
   initState() {
     super.initState();
+    checkLoginStatus();
   }
 
+  checkLoginStatus() async {
+    sharedPreferences = await SharedPreferences.getInstance(); 
+    if(sharedPreferences.getString("token") == null)
+    {
+      Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => ATLoginPage()), (Route<dynamic> route) => false);
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Welcome User")),
+      appBar: AppBar(
+        title: Text("Welcome User"),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () {
+              sharedPreferences.clear(); 
+              sharedPreferences.commit(); 
+            },
+            child: Text("Log Out", style: TextStyle(color: Colors.white)),
+          ),
+        ],
+        ),
       body: Column(
         children: <Widget>[
 
